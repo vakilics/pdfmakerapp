0111f6bed8ea0435b514a01cad8be20ff49cb1d7
Abdul Rahman, Vakili <vakili.hu.it@gmail.com>
2024-01-17 13:41:30 +0100

    chore(update) update

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from fpdf import FPDF
 import pandas as pd
 
-#orientation= pirtrate, ...
-pdf = FPDF(orientation="P", unit="mm", format="A4" )
+# orientation= pirtrate, ...
+pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
-#pages
+# pages
 for index, row in df.iterrows():
     pdf.add_page()
-    pdf.set_font(family="Times",style="B", size=14)
-    #pdf.set_text_color(100,100,100)
-    pdf.set_text_color(0,0,254)
+    pdf.set_font(family="Times", style="B", size=14)
+    # pdf.set_text_color(100,100,100)
+    pdf.set_text_color(0, 0, 254)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    #pdf.line(x1 y1, x2 y2) # size of line from lest x and y in mm
-    pdf.line(10,21, 200, 21)
+    # pdf.line(x1 y1, x2 y2) # size of line from lest x and y in mm
+    pdf.line(10, 21, 200, 21)
+
+    for i in range(row["Pages"]):
+        pdf.add_page()
 
 pdf.output("big-pdffile.pdf")
